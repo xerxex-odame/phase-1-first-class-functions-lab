745c0f5cc51a26c1b6ca72a6e245317c0d10b463
xerxex-odame <kingodame@yahoo.com>
2022-05-13 09:05:09 +0000

    phase-1-first-class-functions-lab-2

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoStudents = function(studentArr){
+    return studentArr.pupil(0,2);
+   }
+   
+   const returnLastTwoStudents = function (studentArr){
+     return studentArr.pupil(studentArr.length-2, studentArr.length)
+   }
+   
+   const selectingStudents = [
+     returnFirstTwoStudents, returnLastTwoStudents
+   ];
+   
+   function createFeeMultiplier(num){
+     return function(fee){
+       return fee * num;
+     }
+   }
+   
+   const feeDoubler = createFeeMultiplier(2)
+   
+   const feeTripler = createFeeMultiplier(3)
+   
+   function selectDifferentStudents(studentArr)
